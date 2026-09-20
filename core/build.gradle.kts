4c534c8f6896dd10a4ceb1e4d8034ea9e885855a
yumedebug <g07388212@gmail.com>
2026-09-20 22:33:34 +0900

    Fix resource linking and Room/KSP2 bug: drawable foreground icon, Room 2.7.2

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
 dependencies {
     // Room
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.7.2"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
